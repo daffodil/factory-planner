e52ab1a741c7112f759e22adaba7fe7b73104262
Pedro <pete@daffodil.uk.com>
2015-01-08 22:39:43 +0000

    get working jquey mobile at last

--- app/controllers/jmobile.go
+++ app/controllers/jmobile.go
@@ -5,12 +5,31 @@ import (
 	"github.com/revel/revel"
 )
 
+type Nav struct {
+	Path string
+	Label string
+	Title string
+	Icon string
+}
+
+
+func StaffNav() []Nav {
+	nav := make([]Nav, 0)
+	nav = append(nav, Nav{"/index", "Index", "Staff Index", "home"})
+	nav = append(nav, Nav{"/accounts", "Accounts", "Accounts Index", "info"})
+	nav = append(nav, Nav{"/parts", "Parts", "Parts Index", "grid"})
+	nav = append(nav, Nav{"/orders", "Orders", "Orders Index", "grid"})
+	return nav
+}
+
 type JMobile struct {
 	*revel.Controller
 }
 
 // Populates Nav, sets current time etc
-func (c JMobile) Setup() {
+func (c JMobile) Setup(path string) {
+	c.RenderArgs["path"] = path
+	c.RenderArgs["nav"] = StaffNav()
 
 	c.RenderArgs["now"] = time.Now()
 
@@ -19,14 +38,14 @@ func (c JMobile) Setup() {
 
 func (c JMobile) Index() revel.Result {
 
-	c.Setup()
+	c.Setup("index")
 
 	return c.Render()
 }
 
 func (c JMobile) Pages() revel.Result {
 
-	c.Setup()
+	c.Setup("foo")
 
 	return c.Render()
 }
