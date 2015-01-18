f2a4b08545a78ba714e12e381fac1013cde512bc
Pedro <pete@daffodil.uk.com>
2015-01-18 01:51:22 +0000

    lets zippy this up a bit

---
+++ app/fp/imports/imports.go
@@ -0,0 +1,13 @@
+
+package imports
+
+/*
+Sage csv
+Excel
+
+Create the files and the import reader
+create import to reader mapping..
+
+Import Map
+
+ */
--- app/fp/imports/sage/sales_orders.go
+++ app/fp/imports/sage/sales_orders.go
@@ -2,3 +2,11 @@
 // Import sage Line 50 Orders
 
 package sage
+
+func ImportSalesOrdersCSV(file_path string) {
+
+
+
+
+
+}
